a549113ba6540c14966dfb982447f2b2ea34b0ad
Alejandro <49820210+Alejandro-H@users.noreply.github.com>
2019-11-06 04:47:58 -0500

    Correción final

--- app/src/main/java/calculadora/tecsup/MainActivity.java
+++ app/src/main/java/calculadora/tecsup/MainActivity.java
@@ -1,24 +1,22 @@
 package calculadora.tecsup;
-
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.annotation.SuppressLint;
+import android.content.DialogInterface;
 import android.os.Bundle;
-
-import android.text.InputType;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import java.text.DecimalFormat;
+import android.widget.PopupMenu;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
-    public EditText contenedor,contenedor2;
-    public boolean inicio=true,igual,op1,op2;
-    public double resultado,valor,valor2;
-    public String funciones,tipoOp;
-    public Button FUNC;
-    public DecimalFormat decimal=new DecimalFormat("0.000");
+    private EditText contenedor,contenedor2;
+    public boolean inicio=true,igual;
+    private double resultado,valor,valor2;
+    private String tipoOp;
+    private Button FUNC;
     @Override
     @SuppressWarnings("SetTextI18n")
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,49 +24,106 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         contenedor=findViewById(R.id.contenedor);
         contenedor2=findViewById(R.id.contenedor2);
+        contenedor.setKeyListener(null);
+        contenedor2.setKeyListener(null);
+        contenedor2.setFocusable(false);
+        contenedor2.setClickable(false);
         FUNC=findViewById(R.id.FUNC);
+        FUNC.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                PopupMenu pmenu=new PopupMenu(getApplicationContext(),FUNC);
+                pmenu.getMenuInflater().inflate(R.menu.popup_menu,pmenu.getMenu());
+                pmenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem item) {
+                        valor=Double.parseDouble(contenedor.getText().toString());
+                        switch (item.getItemId()){
+                            case R.id.sen:
+                                contenedor2.setText("sen ("+valor+")");
+                                tipoOp="sen";
+                                inicio=true;
+                                igual=false;
+                                break;
+                            case R.id.cos:
+                                contenedor2.setText("cos ("+valor+")");
+                                tipoOp="cos";
+                                inicio=true;
+                                igual=false;
+                                return true;
 
+                            case R.id.sec:
+                                contenedor2.setText("sec ("+valor+")");
+                                tipoOp="sec";
+                                inicio=true;
+                                igual=false;
+                                return true;
+
+                            case R.id.csc:
+                                contenedor2.setText("csc ("+valor+")");
+                                tipoOp="csc";
+                                inicio=true;
+                                igual=false;
+                                return true;
+
+                            case R.id.tan:
+                                contenedor2.setText("tan ("+valor+")");
+                                tipoOp="tan";
+                                inicio=true;
+                                igual=false;
+                                return true;
+                            case R.id.cot:
+                                contenedor2.setText("cot ("+valor+")");
+                                tipoOp="cot";
+                                inicio=true;
+                                igual=false;
+                                return true;
+                        }
+                        return false;
+                    }
+                }
+                );
+                pmenu.show();
+                return false;
+            }
+        });
     }
 
     @SuppressWarnings("SetTextI18n")
     public void num1 (View view){
 
-
-
         if (inicio){
             contenedor.setText(" ");
             contenedor.setText("1");
             inicio=false;
-        }else{
-            //noinspection AndroidLintSetTextI18n
+        }else {
             contenedor.setText(contenedor.getText()+"1");
         }
     }
+
+    @SuppressLint("SetTextI18n")
     public void num2 (View view){
-
-
         if (inicio){
             contenedor.setText(" ");
             contenedor.setText("2");
             inicio=false;
         }else{
-            //noinspection AndroidLintSetTextI18n
             contenedor.setText(contenedor.getText()+"2");
         }
     }
 
+    @SuppressLint("SetTextI18n")
     public void num3 (View view){
-
-
         if (inicio){
             contenedor.setText(" ");
             contenedor.setText("3");
             inicio=false;
         }else{
-            //noinspection AndroidLintSetTextI18n
             contenedor.setText(contenedor.getText()+"3");
         }
     }
+
+    @SuppressLint("SetTextI18n")
     public void num4 (View view){
 
 
@@ -77,13 +132,12 @@ public class MainActivity extends AppCompatActivity {
             contenedor.setText("4");
             inicio=false;
         }else{
-            //noinspection AndroidLintSetTextI18n
             contenedor.setText(contenedor.getText()+"4");
         }
     }
+
     @SuppressLint("SetTextI18n")
     public void num5 (View view){
-
 
         if (inicio){
             contenedor.setText(" ");
@@ -152,7 +206,6 @@ public class MainActivity extends AppCompatActivity {
     @SuppressLint("SetTextI18n")
     public void punto (View view){
         if (contenedor.getText().toString().contains(".")){
-            System.out.println("No hacer nada");
         }else{
             contenedor.setText(contenedor.getText()+".");
             inicio=false;
@@ -163,69 +216,30 @@ public class MainActivity extends AppCompatActivity {
     public void Suma (View view){
         igual=true;
         inicio=true;
-        if (op1=true){
-            valor=Double.parseDouble(contenedor.getText().toString());
-            contenedor2.setText(" ");
-            contenedor2.setText(contenedor.getText().toString()+"+");
-            op1=false;
-        }else {
-            if (op2=true){
-                valor=Double.parseDouble(contenedor.getText().toString());
-                contenedor2.setText(" ");
-                contenedor2.setText(contenedor.getText().toString()+"+");
-                op2=false;
-            }else {
-                contenedor2.setText(contenedor.getText()+"+");
-                resolver(resultado,valor2);
-            }
-        }
+        valor=Double.parseDouble(contenedor.getText().toString());
+        contenedor2.setText(" ");
+        contenedor2.setText(contenedor.getText().toString()+"+");
         tipoOp="+";
     }
 
     @SuppressLint("SetTextI18n")
     public void Resta (View view){
-
         igual=true;
         inicio=true;
-        if (op1=true){
-            valor=Double.parseDouble(contenedor.getText().toString());
-            contenedor2.setText(" ");
-            contenedor2.setText(contenedor.getText().toString()+"-");
-            op1=false;
-        }else {
-            if (op2=true){
-                valor=Double.parseDouble(contenedor.getText().toString());
-                contenedor2.setText(" ");
-                contenedor2.setText(contenedor.getText().toString()+"-");
-                op2=false;
-            }else{
-                contenedor2.setText(contenedor.getText()+"-");
-                resolver(resultado,valor2);
-            }
-        }
+        valor=Double.parseDouble(contenedor.getText().toString());
+        contenedor2.setText(" ");
+        contenedor2.setText(contenedor.getText().toString()+"-");
         tipoOp="-";
     }
+
     @SuppressLint("SetTextI18n")
     public void Producto (View view){
 
         igual=true;
         inicio=true;
-        if (op1=true){
-            valor=Double.parseDouble(contenedor.getText().toString());
-            contenedor2.setText(" ");
-            contenedor2.setText(contenedor.getText().toString()+"x");
-            op1=false;
-        }else{
-            if (op2=true){
-                valor=Double.parseDouble(contenedor.getText().toString());
-                contenedor2.setText(" ");
-                contenedor2.setText(contenedor.getText().toString()+"x");
-                op2=false;
-            }else{
-                contenedor2.setText(contenedor.getText()+"x");
-                resolver(resultado,valor2);
-            }
-        }
+        valor=Double.parseDouble(contenedor.getText().toString());
+        contenedor2.setText(" ");
+        contenedor2.setText(contenedor.getText().toString()+"x");
         tipoOp="*";
     }
 
@@ -233,44 +247,10 @@ public class MainActivity extends AppCompatActivity {
     public void Division (View view){
         igual=true;
         inicio=true;
-        if (op1=true){
-            valor=Double.parseDouble(contenedor.getText().toString());
-            contenedor2.setText(" ");
-            contenedor2.setText(contenedor.getText().toString()+"/");
-            op1=false;
-        }else{
-            if (op2=true){
-                valor=Double.parseDouble(contenedor.getText().toString());
-                contenedor2.setText(" ");
-                contenedor2.setText(contenedor.getText().toString()+"/");
-                op2=false;
-            }else{
-                contenedor2.setText(contenedor.getText()+"/");
-                resolver(resultado,valor2);
-            }
-        }
+        valor=Double.parseDouble(contenedor.getText().toString());
+        contenedor2.setText(" ");
+        contenedor2.setText(contenedor.getText().toString()+"÷");
         tipoOp="/";
-
-    }
-
-    @SuppressLint("SetTextI18n")
-    public void Igual (View view){
-
-        inicio=true;
-        op1=true;
-        if (igual=true) {
-            if (tipoOp == null) {
-            } else {
-                valor2 = Double.parseDouble(contenedor.getText().toString());
-                contenedor2.setText(contenedor2.getText().toString() + contenedor.getText().toString());
-                resolver(valor, valor2);
-                igual = false;
-            }
-        }else {
-            contenedor2.setText("");
-            resolver(valor,valor2);
-
-        }
     }
 
     public void Signo (View view){
@@ -279,21 +259,18 @@ public class MainActivity extends AppCompatActivity {
         colector=Double.parseDouble(contenedor.getText().toString());
         numero1=numero2-colector;
         contenedor.setText(String.valueOf(numero1));
-
     }
-    public  void Borrartodo (View view){
 
+    public  void Borrartodo (View view){
         contenedor.setText("0");
         contenedor2.setText("");
         inicio=true;
-        funciones="";
-        op1=true;
-        op2=true;
         igual=true;
         valor=0;
         valor2=0;
         resultado=0;
     }
+
     public void Borrar(View view){
         if (contenedor.getText().length()>0){
             contenedor.setText(contenedor.getText().toString().substring(0,contenedor.getText().length()-1));
@@ -303,15 +280,6 @@ public class MainActivity extends AppCompatActivity {
             }
         }
     }
-    @SuppressLint("SetTextI18n")
-    public void Potencia(View view){
-        inicio=true;
-        valor=Double.parseDouble(contenedor.getText().toString());
-        contenedor2.setText(valor+"^");
-        tipoOp="^";
-        op1=false;
-        igual=false;
-    }
 
     @SuppressLint("SetTextI18n")
     public void Raiz(View view){
@@ -320,63 +288,141 @@ public class MainActivity extends AppCompatActivity {
             contenedor2.setText("√("+valor+")");
             tipoOp="raiz";
             inicio=true;
-            op2=false;
-            op1=false;
             igual=false;
         }else{
-            contenedor.setText("Error de cálculo");
+            Toast.makeText(MainActivity.this,"Error de cálculo",Toast.LENGTH_SHORT).show();
+            contenedor.setText("0");
+            contenedor2.setText("");
+            inicio=true;
         }
     }
+
     public void Salir (View view){
-        finish();
+        AlertDialog.Builder Mensaje=new AlertDialog.Builder(MainActivity.this);
+        Mensaje.setTitle("Calculadora");
+        Mensaje.setMessage("¿Seguro que desea salir de la aplicación?");
+        Mensaje.setPositiveButton("Salir", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                finish();
+
+            }
+            });
+
+        Mensaje.setNegativeButton("No", new DialogInterface.OnClickListener() {
+        @Override
+        public void onClick(DialogInterface dialog, int which) {
+        contenedor.setText("0");
+        inicio=true;
+        }
+            });
+        Mensaje.show();
     }
 
-
-
-
+@SuppressLint("SetTextI18n")
+    public void Potencia(View view){
+        inicio=true;
+        valor=Double.parseDouble(contenedor.getText().toString());
+        contenedor2.setText(valor+"^");
+        tipoOp="^";
+        igual=true;
+    }
 
     @SuppressLint("SetTextI18n")
-    public void resolver(Double va1, Double va2){
+    public void Igual (View view){
+        inicio=true;
+        if (igual=true){
+            if (tipoOp ==null) {
+                contenedor.setText("0");
+            }
+            else {
+                valor2 = Double.parseDouble(contenedor.getText().toString());
+                contenedor2.setText(contenedor2.getText().toString() + contenedor.getText().toString());
+                resolver(valor, valor2);
+                igual = false;
+            }
+        }
+        else {
+            resolver(valor, valor2);
+        }
+    }
 
+    @SuppressLint("SetTextI18n")
+    public void Cuadrado(View view){
+        valor=Double.parseDouble(contenedor.getText().toString());
+        contenedor2.setText(valor+"^2");
+        resultado=Math.pow(valor,2);
+        contenedor.setText(String.valueOf(resultado));
+    }
+
+    @SuppressLint("SetTextI18n")
+    public void resolver(Double valor1, Double valor2){
         switch (tipoOp){
-
             case "+":
-                resultado=va1+va2;
+                resultado=valor1+valor2;
                 contenedor.setText(String.valueOf(resultado));
                 break;
             case "-":
-                resultado=va1-va2;
+                resultado=valor1-valor2;
                 contenedor.setText(String.valueOf(resultado));
                 break;
             case "*":
-                resultado=va1*va2;
+                resultado=valor1*valor2;
                 contenedor.setText(String.valueOf(resultado));
                 break;
             case "/":
-                if (va2==0){
-                    contenedor.setText("Error de división");
+                if (valor2==0){
+                    Toast.makeText(MainActivity.this,"Error de cálculo",Toast.LENGTH_SHORT).show();
+                    contenedor.setText("0");
+                    inicio=true;
                 }else {
-                    resultado = va1 / va2;
+                    resultado = valor1 / valor2;
                     contenedor.setText(String.valueOf(resultado));
                     break;
                 }
-
+                break;
             case "^":
-                resultado=Math.pow(valor,valor2);
+                resultado=Math.pow(valor1,valor2);
                 contenedor.setText(String.valueOf(resultado));
                 break;
+
             case "raiz":
-                resultado=Math.sqrt(valor);
+                contenedor2.setText("√("+valor1+")");
+                resultado=Math.sqrt(valor1);
+                contenedor.setText(String.valueOf(resultado));
+                break;
+
+            case "sen":
+                contenedor2.setText("sen ("+valor1+")");
+                resultado=Math.sin(valor1);
+                contenedor.setText(String.valueOf(resultado));
+                break;
+            case "cos":
+                contenedor2.setText("cos ("+valor1+")");
+                resultado=Math.cos(valor1);
+                contenedor.setText(String.valueOf(resultado));
+                break;
+            case "tan":
+                contenedor2.setText("tan ("+valor1+")");
+                resultado=Math.tan(valor1);
+                contenedor.setText(String.valueOf(resultado));
+                break;
+            case "cot":
+                contenedor2.setText("cot ("+valor1+")");
+                resultado=(1/(Math.tan(valor1)));
+                contenedor.setText(String.valueOf(resultado));
+                break;
+            case "sec":
+                contenedor2.setText("sec ("+valor1+")");
+                resultado=1/(Math.cos(valor1));
+                contenedor.setText(String.valueOf(resultado));
+                break;
+            case "csc":
+                contenedor2.setText("csc ("+valor1+")");
+                resultado=1/(Math.sin(valor1));
                 contenedor.setText(String.valueOf(resultado));
                 break;
         }
     }
-
-
 }
-
-
-
-
-
 
